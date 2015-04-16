61243eb425dd6dddb0e7900874cf0adfbadc8bff
hart6069 <hart6069@vandals.uidaho.edu>
2015-04-16 11:13:54 -0700

    hi

---
+++ build/android/gen/com/asignment4/Assignment4Activity.java
@@ -0,0 +1,7 @@
+package com.asignment4;
+
+import org.appcelerator.titanium.TiRootActivity;
+
+public final class Assignment4Activity extends TiRootActivity
+{
+}
---
+++ build/android/gen/com/asignment4/Assignment4AppInfo.java
@@ -0,0 +1,70 @@
+package com.asignment4;
+
+import org.appcelerator.titanium.ITiAppInfo;
+import org.appcelerator.titanium.TiApplication;
+import org.appcelerator.titanium.TiProperties;
+import org.appcelerator.kroll.common.Log;
+
+/* GENERATED CODE
+ * Warning - this class was generated from your application's tiapp.xml
+ * Any changes you make here will be overwritten
+ */
+public final class Assignment4AppInfo implements ITiAppInfo
+{
+	private static final String LCAT = "AppInfo";
+
+	public Assignment4AppInfo(TiApplication app) {
+	}
+
+	public String getDeployType() {
+		return "development";
+	}
+
+	public String getId() {
+		return "com.asignment4";
+	}
+
+	public String getName() {
+		return "assignment4";
+	}
+
+	public String getVersion() {
+		return "1.0";
+	}
+
+	public String getPublisher() {
+		return "dhart";
+	}
+
+	public String getUrl() {
+		return "http://";
+	}
+
+	public String getCopyright() {
+		return "2015 by dhart";
+	}
+
+	public String getDescription() {
+		return "";
+	}
+
+	public String getIcon() {
+		return "appicon.png";
+	}
+
+	public boolean isAnalyticsEnabled() {
+		return true;
+	}
+
+	public String getGUID() {
+		return "a4c6216d-387a-44a0-b58e-94f239cf783c";
+	}
+
+	public boolean isFullscreen() {
+		return false;
+	}
+
+	public String getBuildType() {
+		return "";
+	}
+}
